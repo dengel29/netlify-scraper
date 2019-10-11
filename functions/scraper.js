8d39b4c762ba3c42c986ca29b7019fc4b3b26193
Dan Engel <dengel29@gmail.com>
2019-10-11 20:37:08 +0800

    .gitignored node_modules

--- functions/scraper.js
+++ functions/scraper.js
@@ -1,7 +1,32 @@
-exports.handler = function(event, context, callback) {
+const chromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
+
+exports.handler = async (event, context) => {
   // your server-side functionality
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
+
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
     });
+ 
+    let page = await browser.newPage();
+ 
+    await page.goto(event.url || 'https://store.steampowered.com/search/?sort_by=Released_DESC&ignore_preferences=1&category1=998');
+ 
+    result = await page.title();
+  } catch (error) {
+    return context.fail(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+ 
+  return context.succeed(result);
 }
